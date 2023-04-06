94658a82a91b357ca0b0adefda92858fc8721626
perasarvani <sarvanipera123@gmail.com>
2023-04-05 19:36:07 -0500

    updated

--- routes/selector.js
+++ routes/selector.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home Page Display' });
+  res.render('selector', { title: 'Selector' });
 });
 
 module.exports = router;
